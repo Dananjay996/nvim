bc99730849f1440716fa638e8496192efad1771b
Dananjay 996 <sreedananjay456@gmail.com>
2024-01-01 15:48:13 +0530

    01-01/2024

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,3 +8,6 @@ vim.cmd("set relativenumber")
 vim.g.mapleader = " "
 
 vim.api.nvim_set_keymap('i', '<C-t>', '<Esc>', { noremap = true })
+
+vim.api.nvim_set_keymap('i', '<C-j>', '<Down>', { noremap = true, expr = false })
+vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', { noremap = true, expr = false })
